2a3f8edf3a9c9b73ab4f5b1f44166725f772c6f8
andrews-ja <andrewsjag@gmail.com>
2024-11-03 21:31:07 +0000

    commented unnecessary test case

--- python/tests/test_all.py
+++ python/tests/test_all.py
@@ -2,5 +2,5 @@ import pytest
 import three_dev
 
 
-def test_sum_as_string():
-    assert three_dev.sum_as_string(1, 1) == "2"
+# def test_sum_as_string():
+#     assert three_dev.sum_as_string(1, 1) == "2"
